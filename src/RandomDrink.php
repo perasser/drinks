71586736f90174fa5cdf64c0708051d8c597c04d
Peter Rasser <peter.rasser@hakzell.at>
2025-12-01 13:01:08 +0100

    add tea

--- src/RandomDrink.php
+++ src/RandomDrink.php
@@ -4,7 +4,7 @@ namespace Perasser\Drinks;
 
 class RandomDrink
 {
-    private array $drinks = ['Wasser', 'Saft'];
+    private array $drinks = ['Wasser', 'Saft', 'Tee'];
 
     public function getDrink(): string {
         return $this->drinks[array_rand($this->drinks)];
